c0e11f3989104e371e8a7cdfdded617ae42e2289
Yotsapat Punyaworapan <110213795+pookantong@users.noreply.github.com>
2023-02-08 16:30:06 +0700

    create Branch.py

--- Code/Event_Discount.py
+++ Code/Event_Discount.py
@@ -1,4 +1,4 @@
-class EventDiscount:
+class EventDiscount():
     def __init__(self, event_name, event_start, event_end, discounted_book, discounted_price):
         self.__event_name = event_name
         self.__event_start = event_start
