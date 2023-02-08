071ffb3c053b35506956f0f00faabae52f419833
Misterpetchz <petch.t1507@gmail.com>
2023-02-09 00:03:31 +0700

    Shipping

---
+++ Code/Shipping.py
@@ -0,0 +1,5 @@
+class Shipping:
+    def __init__(self, purchased_item, order_id, shipping_status):
+        self._purchased_item = purchased_item
+        self._order_id = order_id
+        self._shipping_status = shipping_status
