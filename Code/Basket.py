cd3c11307aa72785460e9eb16b508207f51fae27
Apec <110216400+Rezzpect@users.noreply.github.com>
2023-02-10 21:14:06 +0700

    Update Basket.py

--- Code/Basket.py
+++ Code/Basket.py
@@ -1,5 +1,5 @@
 class Basket:
     def __init__(self, items, pay_method, coupon):
-        self._items_book = items
+        self._items_book = [] #list of items
         self.__pay_method = pay_method
         self.__coupon = coupon
