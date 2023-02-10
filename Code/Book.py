0d11643ace140ea244e7fb6858de8d89809778b5
Apec <110216400+Rezzpect@users.noreply.github.com>
2023-02-10 21:59:01 +0700

    Update Book.py

--- Code/Book.py
+++ Code/Book.py
@@ -1,20 +1,20 @@
 class Book : 
     def __init__(self, product_id, cover, brief, creator, name, book_info, book_ISBN_id, book_publisher, book_preview, 
                  critic_review, table_of_content, summary, genre, date_created, rating, discount, catalog):
-        self.product_id = product_id
-        self.cover = cover
-        self.brief = brief
-        self.creator = creator
-        self.name = name
-        self.book_info = book_info
-        self.book_ISBN_id = book_ISBN_id
-        self.book_publisher = book_publisher
-        self.book_preview = book_preview
-        self.critic_review = critic_review
-        self.table_of_content = table_of_content
-        self.summary = summary
-        self.genre = genre
-        self.date_created = date_created
-        self.rating = rating
-        self.discount = discount
-        self.catalog = catalog
+        self._product_id = product_id
+        self._cover = cover
+        self._brief = brief
+        self._creator = creator
+        self._name = name
+        self._book_info = book_info
+        self._book_ISBN_id = book_ISBN_id
+        self._book_publisher = book_publisher
+        self._book_preview = book_preview
+        self._critic_review = critic_review
+        self._table_of_content = table_of_content
+        self._summary = summary
+        self._genre = genre
+        self._date_created = date_created
+        self._rating = rating
+        self._discount = discount
+        self._catalog = catalog
