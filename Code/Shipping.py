4d19121cfc0661ea70c11be81c18086e9f96db02
Apec <110216400+Rezzpect@users.noreply.github.com>
2023-02-10 21:21:59 +0700

    Update Shipping.py

--- Code/Shipping.py
+++ Code/Shipping.py
@@ -1,5 +1,6 @@
 class Shipping:
-    def __init__(self, purchased_item, order_id, shipping_status):
+    def __init__(self, purchased_item, order_id, shipping_status,payment):
         self._purchased_item = purchased_item
         self._order_id = order_id
         self._shipping_status = shipping_status
+        self._payment = payment
