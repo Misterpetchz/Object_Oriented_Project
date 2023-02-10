5c8914e6e35c83a96e387e0781b3b47a568d06b9
Apec <110216400+Rezzpect@users.noreply.github.com>
2023-02-10 22:15:10 +0700

    Update EventDiscount.py

--- Code/EventDiscount.py
+++ Code/EventDiscount.py
@@ -1,7 +1,8 @@
 class EventDiscount():
-    def __init__(self, event_name, event_start, event_end, discounted_book, discounted_price):
+    def __init__(self, event_name, event_start, event_end, discounted_book, discounted_price, book):
         self._event_name = event_name
         self._event_start = event_start
         self._event_end = event_end
         self._discounted_book = discounted_book
         self._discounted_price = discounted_price
+        self._book = book
