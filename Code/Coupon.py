eb3b633bb000856d087c71cb5e7f99db62287a43
Apec <110216400+Rezzpect@users.noreply.github.com>
2023-02-10 21:19:56 +0700

    Update Coupon.py

--- Code/Coupon.py
+++ Code/Coupon.py
@@ -1,4 +1,5 @@
 class Coupon():
-    def __init__(self, id, expire_date):
+    def __init__(self, id, expire_date,amount):
         self.__id = id
+        self.__amount = amount
         self.__expire_date = expire_date
