f6fa06c52c5a76634b05e228a8eec428ef698afe
Apec <110216400+Rezzpect@users.noreply.github.com>
2023-02-10 22:12:16 +0700

    Update Rating.py

--- Code/Rating.py
+++ Code/Rating.py
@@ -1,5 +1,5 @@
 class Rating:
-    def __init__(self, book, book_rating, book_comment):
-        self._book = book
+    def __init__(self, book_name, book_rating, book_comment):
+        self._book_name = book_name
         self._book_rating = book_rating
         self._book_comment = book_comment
