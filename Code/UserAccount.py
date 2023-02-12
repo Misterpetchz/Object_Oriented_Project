fbd02dfda4ff1a02ab8d05a1577224c3f6c85b09
Misterpetchz <petch.t1507@gmail.com>
2023-02-12 23:11:27 +0700

    delete self in super

--- Code/UserAccount.py
+++ Code/UserAccount.py
@@ -10,13 +10,13 @@ class UserAccount:
 # Inheritance from UserAccount
 
 class Customer(UserAccount):
-    def __init__(self, email, password, full_name, gender, tel, address, email_notification, sms_notification):
-        super().__init__(self, email, password, full_name, gender, tel, shipping)
+    def __init__(self, email, password, full_name, gender, tel, shipping, address, email_notification, sms_notification):
+        super().__init__(email, password, full_name, gender, tel, shipping)
         self._address = address
         self.__email_notification = email_notification
         self.__sms_notification = sms_notification
 
 class Admin(UserAccount):
-    def __init__(self, email, password, full_name, gender, tel, permission):
-        super().__init__(self, email, password, full_name, gender, tel, shipping)
+    def __init__(self, email, password, full_name, gender, tel, shipping, permission):
+        super().__init__(email, password, full_name, gender, tel, shipping)
         self.__permission = permission
