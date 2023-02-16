1f74cfd018b98ef53ebd611fc165fd0d48b78ad8
Misterpetchz <petch.t1507@gmail.com>
2023-02-16 23:45:03 +0700

    update

--- testqr.py
+++ testqr.py
@@ -1,12 +1,12 @@
 from promptpay import qrcode
 
 # generate a payload
-id_or_phone_number = "0980231172"
+id_or_phone_number = "0890767442"
 payload = qrcode.generate_payload(id_or_phone_number)
-payload_with_amount = qrcode.generate_payload(id_or_phone_number, 100)
+payload_with_amount = qrcode.generate_payload(id_or_phone_number, 1000)
 
 # export to PIL image
 img = qrcode.to_image(payload)
 
 # export to file
-qrcode.to_file(payload_with_amount, "./qrcode-0980231172.png") 
+qrcode.to_file(payload_with_amount, "./qrcode-0890767442.png") 
