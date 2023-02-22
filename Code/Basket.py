08785c976046f514e9387c92e287b895493e2053
Yotsapat Punyaworapan <110213795+pookantong@users.noreply.github.com>
2023-02-22 16:45:36 +0700

    last update class diagram code

--- Code/Basket.py
+++ Code/Basket.py
@@ -1,5 +1,3 @@
 class Basket:
-    def __init__(self, items, pay_method, coupon):
-        self._items_book = [] #list of items
-        self.__pay_method = pay_method
-        self.__coupon = coupon
+    def __init__(self, book_item):
+        self.__book_item = book_item
---
+++ Code/CreditCard.py
@@ -0,0 +1,5 @@
+class Creditcard:
+    def __init__(self, card_num, expire_date, cvc):
+        self.__card_num = card_num
+        self.__expire_date = expire_date
+        self.__cvc = cvc
