98e46ae2aff8ff5172dcdc8afbe11eb0aa64520e
Salted_Boi <oo2018oo@Laptop-29618>
2023-03-18 21:41:03 +0700

    <code+>: add example instance of Admin, Customer, Book, Branch

---
+++ Code/EarthInstanceExample/Book1.py
@@ -0,0 +1,36 @@
+from Book import BookItem
+from EventDiscount import EventDiscount
+from Rating import Rating
+
+book1 = BookItem(78254,
+                 "cover78254.png",
+                 "Leave now, Human. You don't know what you are facing",
+                 "Eoin Colfer",
+                 "Artemis Fowl",
+                 "296 page Hardcover",
+                 9749000862,
+                 "Tree Publishing",
+                 "One day...",
+                 "Great Story",
+                 [],
+                 "He found something",
+                 ["Fantasy", "SCI-FI"],
+                 "14-Dec-01",
+                 Rating(),
+                 EventDiscount())
+book2 = BookItem(58488,
+                 "cover58488.png",
+                 "Welcome to the mysterious world of god.",
+                 "Cosmos",
+                 "Myth of God and Inhuman",
+                 "147 page Hardcover",
+                 9748241734,
+                 "Phailin Publishing",
+                 "One day....",
+                 "Interesting Book",
+                 [],
+                 "The god have been angered",
+                 ["Fantasy", "Historical"],
+                 "17-Nov-01",
+                 Rating(),
+                 EventDiscount())
---
+++ Code/EarthInstanceExample/Customer1.py
@@ -0,0 +1,11 @@
+from UserAccount import Customer
+
+customer1 = Customer("LLL@gmail.com", 
+                      "giDruGboEEG", 
+                      "Giorno Giovanna", 
+                      "Male", 
+                      "1234567890", 
+                      [], 
+                      "Nonthaburi Pakkret", 
+                      False, 
+                      False)
