22a6a4fabe362f0758d7152d4069f7157101bafc
Misterpetchz <petch.t1507@gmail.com>
2023-03-18 21:38:50 +0700

    <code+> : bookExample

---
+++ Code/petchExample/bookPetch.py
@@ -0,0 +1,39 @@
+from Book import BookItem
+from Rating import Rating
+from EventDiscount import EventDiscount
+
+# 280 bant
+book1 = BookItem(4090,
+                 'Element_Online_Phase4_1.png',
+                 'online game',
+                 'MASALAN',
+                 'Element Online Phase 4.1',
+                 'Hard cover with 388 page and 8 game card',
+                 '978-616-00-2417-9',
+                 'satapornbooks',
+                 '1.....',
+                 'Good Novel',
+                 [],
+                 'เกม EO อัปเดตแพตช์ที่มาพร้อมเมืองใหม่ นั้นคือ เกาะลอยฟ้า',
+                 'fantasy',
+                 '18-Mar-2016',
+                 Rating,
+                 EventDiscount)
+
+# 170 bant
+book2 = BookItem(1080,
+                 'แค่ใช้คำให้ฉลาด.png',
+                 'tips',
+                 'ซาซากิ เคอิจิ',
+                 'แค่ใช้คำให้ฉลาดก็เพิ่มโอกาสจาก 0 เป็น 100',
+                 'Hard cover with 214 page',
+                 '978-616-287-089-7',
+                 'วีเลิร์น',
+                 '1......',
+                 'Good book tips',
+                 [],
+                 'การใช้คำให้ฉลาดจะสามารถเพิ่มโอกาสได้',
+                 'self-help book',
+                 '1-Jan-2013',
+                 Rating,
+                 EventDiscount)
