e187e8231592befadc2cc27f85b3699ed9c51100
Yotsapat Punyaworapan <110213795+pookantong@users.noreply.github.com>
2023-03-18 21:47:03 +0700

    <code+>Pookan[Admin,Customer,BookItem,Branch]

---
+++ Code/Pookan_instance/PookanCustomer.py
@@ -0,0 +1,11 @@
+from UserAccount import Customer
+pookaneiei = Customer('pookantong.p@gmail.com',
+                 'PomyukmeFan555',
+                 'PookanNaja',
+                 'Male',
+                 '0980231173',
+                 [],
+                 '29/7 หมู่2 ตำบลบั้นเด้า อำเภอรถแห่ จังหวัดสก๊อย ประเทศหิวข้าว ดาวSun',
+                 True,
+                 True
+)
